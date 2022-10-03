c36313ca2fd8901942eb32e12e980e7abed98c92
Dmitriy Golovin <hollapuppy@altlinux.org>
2022-10-03 21:18:21 +0300

    rework func for speed up work

--- get_diff_branches/core.py
+++ get_diff_branches/core.py
@@ -4,7 +4,6 @@ import json
 def request(b1: str, a1:str, b2: str, a2: str):
     url='https://rdb.altlinux.org/api/export/branch_binary_packages/'
     branch_list = ['sisyphus', 'sisyphus_e2k', 'sisyphus_mipsel', 'sisyphus_riscv64', 'p10', 'p10_e2k', 'p9', 'p9_e2k', 'p9_mipsel', 'p8', 'c9f2', 'c7']
-
     if (b1 and b2) not in branch_list:
         print('Bad branch')
     else: 
@@ -16,34 +15,42 @@ def request(b1: str, a1:str, b2: str, a2: str):
         else:
             package_list_b1 = (json.loads(request_b1.text))["packages"]
             package_list_b2 = (json.loads(request_b2.text))["packages"]
-            
-            short_package_list_1 = list(map(lambda row: row['name'],package_list_b1))
-            short_package_list_2 = list(map(lambda row: row['name'],package_list_b2))
-            print('1')
-            package_list_diff_b1 = [x for x in short_package_list_1 if x not in short_package_list_2]
+
+            short_package_list_b1 = list(map(lambda row: row['name'],package_list_b1))
+            short_package_list_b2 = list(map(lambda row: row['name'],package_list_b2))
+            result_list_packages_1_wihout_2=list(set(short_package_list_b1) - set(short_package_list_b2))
+            result_list_packages_2_wihout_1=list(set(short_package_list_b2) - set(short_package_list_b1))
+            result_list_of_same_packages=list(set(short_package_list_b1) & set(short_package_list_b2))
+
+            full_list_1 = []
+            for package in (result_list_packages_1_wihout_2):
+                full_list_1.append({'name': package})
             with open('./work_files/b1.txt', 'w') as file1:
-                file1.write(str(dict(qount=len(package_list_diff_b1),packages=package_list_diff_b1)))
-            print('2')
-            package_list_diff_b2 = [x for x in short_package_list_2 if x not in short_package_list_1]
+                file1.write(str(dict(count=len(result_list_packages_1_wihout_2),packages=full_list_1)))
+
+            full_list_2 = []
+            for package in (result_list_packages_2_wihout_1):
+                full_list_2.append({'name': package})
             with open('./work_files/b2.txt', 'w') as file2:
-                file2.write(str(dict(qount=len(package_list_diff_b2),packages=package_list_diff_b2)))
-            print('3')
-            short_package_list_1_v = list(map(lambda row: dict(name=row['name'], version=row['version']),package_list_b1))
-            short_package_list_2_v = list(map(lambda row: dict(name=row['name'], version=row['version']),package_list_b2))
-            print('4')
-            result_list = []
+                file2.write(str(dict(count=len(result_list_packages_2_wihout_1),packages=full_list_2)))
+            dict1={}
+            for package in package_list_b1:
+                dict1[package['name']] = package['version']
 
-            for i in short_package_list_1_v:
-                for j in short_package_list_2_v:
-                    if i['name'] == j['name'] and i['version'] > j['version']:
-                        result_list.append(i)
+            dict2={}
+            for package in package_list_b2:
+                dict2[package['name']] = package['version']
 
-            with open('./work_files/result.txt', 'w') as file2:
-                file2.write(str(result_list))
+            answer = []
+            for moduleName in result_list_of_same_packages:
+                if dict1[moduleName] > dict2[moduleName]:
+                    answer.append({'name':moduleName, 'version_b1':dict1[moduleName], 'version_b2':dict2[moduleName]})
 
+            with open('./work_files/result.txt', 'w') as file3:
+                file3.write(str(dict(count=len(answer),packages=answer)))
 
 request('p10', 'x86_64', 'p9', 'x86_64') 
 
 
-
+                    
 
--- get_diff_branches/test.py
+++ get_diff_branches/test.py
@@ -1,14 +1,27 @@
-import re
 
 
 def request(b1: str, a1:str, b2: str, a2: str):
-    result_list = [{'name': '0ab', 'version': '0.0.23b'}, {'name': '0ad', 'version': '0.0.25b'}, {'name': '0ad-debuginfo', 'version': '0.0.21b'}, {'name': '389-ds-base', 'version': '1.4.1.19'}]
-    result_list2 = [{'name': '0ad', 'version': '0.0.23b'}, {'name': '0ad-debuginfo', 'version': '0.0.21b'}, {'name': '389-ds-base', 'version': '1.4.1.18'}]
-    for i in result_list:
-        for j in result_list2:
-            if i['name'] == j['name'] and i['version'] > j['version']:
-                print(i)
+    result_names = ['0ad', '0ad-debuginfo', '389-ds-base', '389-ds-base-debuginfo', '389-ds-base-devel', '389-ds-base-libs-debuginfo']
+    result_list1 = [{'0ad': '0.0.26'}, {'0ad-debuginfo': '0.0.26'}, {'389-ds-base': '1.4.3.25'}, {'389-ds-base-debuginfo': '1.4.3.25'}, {'389-ds-base-devel': '1.4.3.25'}, {'389-ds-base-libs': '1.4.3.25'}, {'389-ds-base-libs-debuginfo': '1.4.3.25'}]
+    result_list2 = [{'0ad': '0.0.25'}, {'0ad-debuginfo': '0.0.26'}, {'389-ds-base': '1.4.3.23'}, {'389-ds-base-debuginfo': '1.4.3.25'}, {'389-ds-base-devel': '1.4.3.28'}, {'389-ds-base-libs': '1.4.3.21'}, {'389-ds-base-libs-debuginfo': '1.4.3.16'}]
+    answer = []
 
+    for elem in result_names:
+        VersionFrom_resultList1 = ""
+        VersionFrom_resultList2 = ""
 
+        for dict in result_list1:
+            if elem in dict.keys():
+                VersionFrom_resultList1 = dict[elem]
+                break
 
+        for dict in result_list2:
+            if elem in dict.keys():
+                VersionFrom_resultList2 = dict[elem]
+                break
+
+        if VersionFrom_resultList1 != "" and VersionFrom_resultList2 !="":
+            if VersionFrom_resultList1 > VersionFrom_resultList2:
+                answer.append({'name':elem, 'version_b1':VersionFrom_resultList1, 'version_b2':VersionFrom_resultList2})
+    print(answer)
 request('p10', 'x86_64', 'p9', 'x86_64') 
