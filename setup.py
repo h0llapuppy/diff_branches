b438673a23fe923e1807be4c4fa23e3a76c488d2
Dmitriy Golovin <hollapuppy@altlinux.org>
2022-10-05 15:58:56 +0300

    reconf gitignore

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname\
+
+import get_diff_branches
+
+setup(
+    name='get_diff_branches',
+    version=get_diff_branches.__version__,
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+
+    entry_points={
+        'console_scripts':
+            ['get-diff-branches = get_diff_branches.core:request']
+        }
+)
